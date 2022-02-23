0c4bf6a9dc9df7975799f153a72c4e8b1810efe6
rajeshwari1101 <rajeshwaripatil1101@gmail.com>
2022-02-23 13:49:56 +0530

    Adding length calculation program

---
+++ Line_Comparison_Problem/LineComparison.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparison_Problem
+{
+    internal class LineComparison
+    {
+
+        public int X_1, X_2, Y_1, Y_2, S_1, S_2, R_1, R_2;
+        public void length_line()
+        {
+
+            Console.WriteLine("Enter x, y co ordinates of the line 1 :");
+            Console.WriteLine("Enter value X_1");
+            X_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter vale X_2");
+            X_2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter vale Y_One");
+            Y_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter vale Y_2");
+            Y_2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter x, y co ordinates of the line 2 :");
+            Console.WriteLine("Enter value of S_1");
+            S_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of S_2");
+            S_2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter vale R_1");
+            R_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter vale R_Two");
+            R_2 = Convert.ToInt32(Console.ReadLine());
+
+            double Length_XY = Math.Sqrt(Math.Pow((X_1 - X_2), 2) + Math.Pow((Y_2 - Y_1), 2));
+            Console.WriteLine("Length of Line 1 is:: " + Length_XY);
+
+            double Length_SR = Math.Sqrt(Math.Pow((S_2 - S_1), 2) + Math.Pow((R_2 - R_1), 2));
+            Console.WriteLine("Length of Line 2 is: " + Length_SR);
+            Console.ReadLine();
+
+        }
+       
+    }
+}
--- Line_Comparison_Problem/Program.cs
+++ Line_Comparison_Problem/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparison_Problem 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Problems");
+            Console.ReadLine();
+
+            LineComparison line = new LineComparison();
+            line.length_line();
+        }
+    }
+}
